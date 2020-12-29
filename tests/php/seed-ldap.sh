f42201cab887ae77d70969edb6f8d5fd6c37eecb
William Desportes <williamdes@wdes.fr>
2020-12-29 21:21:32 +0100

    Add the org level to seeds

--- tests/php/seed-ldap.sh
+++ tests/php/seed-ldap.sh
@@ -7,6 +7,8 @@ cd $(dirname $0)/../
 CONTAINER_NAME=ldap.mail.williamdes.eu.org
 docker exec -i ${CONTAINER_NAME} ldapwhoami -ZZ -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin
 
+docker exec -i ${CONTAINER_NAME} ldapadd -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin < org.ldiff
+
 docker exec -i ${CONTAINER_NAME} ldapadd -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin < email1.ldiff
 
 docker exec -i ${CONTAINER_NAME} ldapadd -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin < email2.ldiff
