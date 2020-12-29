8d07f20239a9391bbbf3b54bba71f151f8d5dd67
William Desportes <williamdes@wdes.fr>
2020-12-29 21:07:18 +0100

    Seed ldap test users and keep exit code from run-tests

---
+++ tests/php/seed-ldap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)/../
+
+CONTAINER_NAME=ldap.mail.williamdes.eu.org
+docker exec -i ${CONTAINER_NAME} ldapwhoami -ZZ -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin
+
+docker exec -i ${CONTAINER_NAME} ldapadd -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin < email1.ldiff
+
+docker exec -i ${CONTAINER_NAME} ldapadd -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin < email2.ldiff
+
+docker exec -i ${CONTAINER_NAME} ldapsearch -LLL -Z -h localhost -D "cn=admin,dc=mail,dc=williamdes,dc=eu,dc=org" -w PasswordLdapAdmin "*" -b "dc=mail,dc=williamdes,dc=eu,dc=org"
