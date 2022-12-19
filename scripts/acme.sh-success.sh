ab6d181832a3d6c2cf6f86240767af1d9db0a034
William Desportes <williamdes@wdes.fr>
2022-12-19 20:32:08 +0100

    Only mirror .cer and .key files

--- scripts/acme.sh-success.sh
+++ scripts/acme.sh-success.sh
@@ -3,8 +3,10 @@
 set -e
 
 # shellcheck disable=SC2154
-rm -rf "/acme.sh/${DOMAIN_NAME}-mirror1"
-cp -rp "/acme.sh/${DOMAIN_NAME}/" "/acme.sh/${DOMAIN_NAME}-mirror1"
+rm -rvf "/acme.sh/${DOMAIN_NAME}-mirror1"
+mkdir -p "/acme.sh/${DOMAIN_NAME}-mirror1"
+cp -v "/acme.sh/${DOMAIN_NAME}/"*.cer "/acme.sh/${DOMAIN_NAME}-mirror1"
+cp -v "/acme.sh/${DOMAIN_NAME}/"*.key "/acme.sh/${DOMAIN_NAME}-mirror1"
 echo 'Mirrors are done.'
 
 chmod 777 -R "/acme.sh/${DOMAIN_NAME}-mirror1"
